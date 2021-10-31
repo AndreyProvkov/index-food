8a89f9e3c6146e1accf14d78ccf9d3cbfafe69a6
AndreyProvkov <40251997+AndreyProvkov@users.noreply.github.com>
2021-11-01 00:12:57 +0500

    Adding content to a block "Delivery"

---
+++ js/main.js
@@ -0,0 +1,12 @@
+// Initialize Swiper
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+});
